e555ae7a9c2f9afb8869780ee8470cff0fc1974d
João Arthur Silva <joaoartsilva@gmail.com>
2022-11-23 21:15:58 -0300

    Fixed logic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -26,26 +26,35 @@ public class GameController : MonoBehaviour
         var espacamento = Random.Range(.2f, .4f);
         var espacamentoAtualX = espacamento;
         var espacamentoAtualY = espacamento;
+        Debug.Log(largura + " " + altura);
+
         for (var i = 0; i < largura; i++)
         {
-            var go1 = new GameObject
-            {
-                transform =
-                {
-                    position = new Vector3(espacamentoAtualX, 0)
-                }
-            };
-            go1.AddComponent<>();
+            var go1 = CreateHex(espacamentoAtualX, espacamentoAtualY);
+
+            var go1Sprite = go1.AddComponent<SpriteRenderer>().sprite = _spriteHexagon;
             for (var j = 0; j < altura; j++)
             {
-                var go2 = new GameObject
-                {
-                    transform =
-                    {
-                        position = new Vector3(0, espacamentoAtualY)
-                    }
-                };
+                var go2 = CreateHex(espacamentoAtualX, espacamentoAtualY);
+                var go2Sprite = go2.AddComponent<SpriteRenderer>().sprite = _spriteHexagon;
+                espacamentoAtualY += go2Sprite.bounds.extents.y * 2;
             }
+
+            espacamentoAtualY = espacamento;
+            espacamentoAtualX += go1Sprite.bounds.extents.x * 2;
         }
+    }
+
+    private GameObject CreateHex(float espacamentoAtualX, float espacamentoAtualY)
+    {
+        var go1 = new GameObject
+        {
+            name = "Hex",
+            transform =
+            {
+                position = new Vector3(espacamentoAtualX, espacamentoAtualY)
+            }
+        };
+        return go1;
     }
 }
